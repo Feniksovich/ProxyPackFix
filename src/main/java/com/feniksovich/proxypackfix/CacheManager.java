b4031057b5c7b443acdaf4ae402afc89c1fd871e
Feniksovich <maxsimus.i@yandex.ru>
2022-07-08 22:22:03 +0600

    Added multiple resource packs per player support

--- src/main/java/com/feniksovich/proxypackfix/CacheManager.java
+++ src/main/java/com/feniksovich/proxypackfix/CacheManager.java
@@ -1,25 +1,27 @@
 package com.feniksovich.proxypackfix;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 public class CacheManager {
 
     private final ProxyPackFix plugin;
-    private final Map<UUID, String> cache = new HashMap<>();
+    private final Map<UUID, Set<String>> cache = new HashMap<>();
 
     public CacheManager(ProxyPackFix plugin) {
         this.plugin = plugin;
     }
 
     public void add(final UUID uuid, final String hash) {
-        cache.put(uuid, hash);
+        if (cache.containsKey(uuid)) {
+            cache.get(uuid).add(hash);
+        } else {
+            cache.put(uuid, Set.of(hash));
+        }
         plugin.debugLog("Added cache record for " + uuid + " with hash " + hash);
     }
 
-    public boolean isSame(final UUID uuid, final String hash) {
-        return cache.containsKey(uuid) && cache.get(uuid).equals(hash);
+    public boolean isPresent(final UUID uuid, final String hash) {
+        return cache.containsKey(uuid) && cache.get(uuid).contains(hash);
     }
 
     public void remove(final UUID uuid) {
