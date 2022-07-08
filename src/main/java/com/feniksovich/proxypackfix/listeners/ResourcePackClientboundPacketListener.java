d8b62b637ee7ae46c66928397b078c55b306fb53
Feniksovich <maxsimus.i@yandex.ru>
2022-07-08 22:23:43 +0600

    Added empty hash check and related warning message with disabling caching

--- src/main/java/com/feniksovich/proxypackfix/listeners/ResourcePackClientboundPacketListener.java
+++ src/main/java/com/feniksovich/proxypackfix/listeners/ResourcePackClientboundPacketListener.java
@@ -21,7 +21,19 @@ public class ResourcePackClientboundPacketListener extends AbstractPacketListene
     @Override
     public void packetReceive(PacketReceiveEvent<ResourcePackClientboundPacket> e) {
         plugin.debugLog("PacketReceived: " + e.packet().toString());
-        if (plugin.getCacheManager().isSame(e.player().uniqueId(), e.packet().getHash())) {
+
+        if (e.packet().getHash().isEmpty() || e.packet().getHash() == null) {
+            plugin.getLogger().warning("§e= = = = = = WARNING = = = = = =");
+            plugin.getLogger().warning("§eSent resource pack has no hash. Caching will be disabled for this resource pack.");
+            plugin.getLogger().warning("§eProxyPackFix features won't work for this resource pack due to inability to uniquely identify it.");
+            plugin.getLogger().warning("§eYou must specify a hash for this resource pack to fix this.");
+            plugin.getLogger().warning("§eResource Pack URL: " + e.packet().getUrl());
+            plugin.getLogger().warning("§e= = = = = = WARNING = = = = = =");
+
+            return;
+        }
+
+        if (plugin.getCacheManager().isPresent(e.player().uniqueId(), e.packet().getHash())) {
             plugin.debugLog("Cached record found for " + e.player().uniqueId() + " (hash " + e.packet().getHash() + ")");
             plugin.debugLog("Hash the same, skipping installation of resource pack.");
 
